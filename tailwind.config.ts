990a4a2633c3231743cd5bdd7e09da287e86e3f0
mfurkanylmaz <mfurkanyilmaz2020@gmail.com>
2025-08-03 22:48:06 +0300

    Create tailwind.config.ts

---
+++ tailwind.config.ts
@@ -0,0 +1,19 @@
+import { type Config } from "tailwindcss";
+import defaultTheme from "tailwindcss/defaultTheme";
+
+const config: Config = {
+  content: [
+    "./app/**/*.{ts,tsx,js,jsx}",
+    "./components/**/*.{ts,tsx,js,jsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Inter", ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  plugins: [],
+};
+
+export default config;
